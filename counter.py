17e8209e31f28c3f2a6e0043087df133aaf368b3
Milos Stankovic <mjashica@gmail.com>
2024-01-31 18:40:15 +0100

    Create counter.py
    initial commit

---
+++ counter.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify, request, render_template
+
+app = Flask(__name__)
+
+counter = 0
+
+@app.route('/')
+def index():
+    return render_template('index.html', counter=counter)
+
+@app.route('/get', methods=['GET'])
+def get_counter():
+    return jsonify({'counter': counter})
+
+@app.route('/add', methods=['POST'])
+def update_counter():
+    global counter
+    counter += 1
+    return jsonify({'counter': counter})
+
+if __name__ == '__main__':
+    app.run(debug=True)
